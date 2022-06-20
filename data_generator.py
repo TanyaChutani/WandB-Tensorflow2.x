a5a99857c1473d9a02edbff1c5c518b3c82e114d
Tanya <tanyach1997@gmail.com>
2022-06-20 10:20:58 +0530

    added __data_generation method

--- data_generator.py
+++ data_generator.py
@@ -15,6 +15,7 @@ class DataGenerator(tf.keras.utils.Sequence):
         self.on_epoch_end()
 
     def __load_dataset(self):
+
         (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
         self.images, self.labels = (
@@ -26,10 +27,10 @@ class DataGenerator(tf.keras.utils.Sequence):
 
     def __getitem__(self, index):
         indexes = self.indexes[index * self.batch_size : (index + 1) * self.batch_size]
-        image_keys_temp = [self.images[k] for k in indexes]
+        images = [self.images[k] for k in indexes]
         labels = [self.labels[k] for k in indexes]
-        images = self.__preprocess(image_keys_temp)
-        return images, labels
+        batch_images, batch_labels = self.__data_generation(images, labels)
+        return batch_images, batch_labels
 
     def on_epoch_end(self):
         self.indexes = tf.range((self.images).shape[0])
@@ -43,3 +44,18 @@ class DataGenerator(tf.keras.utils.Sequence):
         image = image / 255.0
         image = tf.cast(image, tf.float32)
         return image
+
+    def __data_generation(self, images, labels):
+        batch_images = np.empty(
+            (self.batch_size, self.resize_dim[0], self.resize_dim[1], 3), dtype="int"
+        )
+        batch_labels = np.empty((self.batch_size, 1), dtype="float32")
+        for idx, (image, label) in enumerate(zip(images, labels)):
+            batch_images[
+                idx,
+            ] = self.__preprocess(image)
+            batch_labels[
+                idx,
+            ] = label
+
+        return batch_images, batch_labels
